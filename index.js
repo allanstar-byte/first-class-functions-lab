d096f009fa433b2a8f8137e3f9cbe1672edf9d89
allanstar-byte <allanmutisya263@gmail.com>
2022-06-12 13:32:25 +0300

    Try

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+
+// const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function() {
+    // const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    arr = drivers.slice(0, 2)
+    console.log(arr)
+}
+
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+
+const returnLastTwoDrivers = function() {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    consol.log(drivers.slice[drivers.length - 2])
+}
+returnLastTwoDrivers()
